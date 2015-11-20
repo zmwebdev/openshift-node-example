540fd3be81cc9335d0841e78ab30f7f0130c2b69
zmwebdev <zmwebdev@gmail.com>
2015-11-20 17:12:42 +0000

    first

---
+++ app.js
@@ -0,0 +1,9 @@
+// $ node app.js
+
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080);
